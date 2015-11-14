784120e52ac9cfea3655ea1bd48216922780df32
hbindervitti <bindervh@oregonstate.edu>
2015-11-14 08:08:35 -0800

    wk 7 hw

---
+++ wk7hw/requests.js
@@ -0,0 +1,64 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.post('/', function(req, res){
+	var context = readData('GET', req, false);
+	context.post = readData('POST', req, true);
+	context.isPost = true;
+	res.render('displayReq', context);
+});
+
+app.get('/', function(req, res){
+	var context = readData('Get', res, false);
+	context.isPost = false;
+	res.render('displayReq', context);
+});
+
+var readData = function(type, req, isPost){
+if(isPost)
+{
+	var qParams = [];
+	for (var p in req.body){
+		qParams.push({'name':p,'value':req.body[p]})
+	}
+	console.log(qParams);		//debug
+	console.log(req.body);	//debug
+	var context = {};
+	context.dataList = qParams;
+}
+else
+{
+	var qParams = [];
+	for (var p in req.query){
+		qParams.push({'name':p,'value':req.query[p]})
+	}
+	var context = {};
+	context.dataList = qParams;
+}
+};
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
