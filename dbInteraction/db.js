103cb3236b8a84daf474026d70370ac907978d25
hbindervitti <bindervh@oregonstate.edu>
2015-12-02 17:16:52 -0800

    db work

--- dbInteraction/db.js
+++ dbInteraction/db.js
@@ -19,7 +19,7 @@ app.get('/',function(req,res,next){
       next(err);
       return;
     }	
-    context.results = JSON.stringify(rows);
+    //context.results = JSON.stringify(rows);
 	
 	var myData = [];
 	for(var i in rows){
