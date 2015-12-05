0b37235c53af75afd0c63948ac16fa71a58e2758
hbindervitti <bindervh@oregonstate.edu>
2015-12-05 15:51:32 -0800

    db stuff

--- HW10/app.js
+++ HW10/app.js
@@ -25,7 +25,7 @@ app.get('/',function(req,res,next){
 	context.results = rows;
 	// console.log(context.results);
     // res.render('home', context);
-	res.send('/test.html');
+	res.sendfile('/test.html');
   }); 
 });
 
