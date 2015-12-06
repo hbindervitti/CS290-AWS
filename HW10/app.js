4faa2947476663aa48d54929a285a4191a52144c
hbindervitti <bindervh@oregonstate.edu>
2015-12-05 16:07:32 -0800

    db stuff

--- HW10/app.js
+++ HW10/app.js
@@ -2,15 +2,15 @@ var express = require('express');
 var mysql = require('./dbconn.js');
 
 var app = express();
-// var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// app.engine('handlebars', handlebars.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', 3000);
 
 app.get('/',function(req,res,next){
@@ -25,7 +25,7 @@ app.get('/',function(req,res,next){
 	context.results = rows;
 	// console.log(context.results);
     // res.render('home', context);
-	res.render('test.html');
+	res.render('test', context);
   }); 
 });
 
