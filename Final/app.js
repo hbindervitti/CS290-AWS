669fce4635ab77638b33b6cc0fc7a31e96ff560b
hbindervitti <bindervh@oregonstate.edu>
2015-12-06 11:45:08 -0800

    db stuff

--- Final/app.js
+++ Final/app.js
@@ -76,7 +76,7 @@ app.post('/api/workout/:id',function(req,res,next){
 
 app.get('/reset-table',function(req,res,next){
   // var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
+  mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){
     var createString = "CREATE TABLE workouts("+
     "id INT PRIMARY KEY AUTO_INCREMENT,"+
     "name VARCHAR(255) NOT NULL,"+
